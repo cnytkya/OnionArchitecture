98b59c115a7555698e865759115908327dffa0d7
cnytkya <kayacuneyt2542@gmail.com>
2025-02-20 23:59:04 +0300

    Final

--- Core/OnionArchitecture.Domain/Entities/Blog.cs
+++ Core/OnionArchitecture.Domain/Entities/Blog.cs
@@ -3,10 +3,6 @@
     public class Blog
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string? Content { get; set; }
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
-        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string? Title { get; set; }
     }
 }
--- Core/OnionArchitecture.Domain/Entities/Category.cs
+++ Core/OnionArchitecture.Domain/Entities/Category.cs
@@ -3,8 +3,6 @@
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int OrderNo { get; set; }
-        public IEnumerable<Blog> Blogs { get; set; }
+        public string? Name { get; set; }
     }
 }
